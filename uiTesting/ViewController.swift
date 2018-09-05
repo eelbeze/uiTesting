4f35d4f881abdc5b27475074d997bc8547dcfe2d
eelbeze <elise.elbeze@gmail.com>
2018-09-05 14:47:41 +0200

    doc: add Xcode project

---
+++ uiTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uiTesting
+//
+//  Created by Elise El beze on 05/09/2018.
+//  Copyright © 2018 Elise El beze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
